e50a5622f2b7c023e44117acadb85afaede9d3b6
kamalavasan18-cell <kamalavasan18@gmail.com>
2026-07-04 21:48:11 +0530

    Enhance guessing game with attempt limits and error handling
    Added attempt tracking and maximum attempts limit to the guessing game.

--- game.py
+++ game.py
@@ -1,13 +1,22 @@
 import random 
 secret=random.randint(1,100)
+attempt=0
+max_attempts=5
 
-while True:
-    guess=int(input("enter number between 1-100: "))
-    if guess<secret:
-        print("very low")
-    elif guess>secret:
-        print("too high")
-    else:
-        print("you won budddy")
-        break
- 
+while attempt<max_attempts:
+    try:
+        guess=int(input("enter number between 1-100: "))
+        attempt+=1
+        if guess<secret:
+            print("very low")
+        elif guess>secret:
+            print("too high")
+     
+        else:
+            print("you won buddy and the correct answer is,{attempt}")
+    except ValueError:
+            print("print only the numbers")
+            break
+
+if attempt == max_attempts:
+    print(" OOPS! you have reached the limit") 
